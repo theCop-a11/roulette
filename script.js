f1d4265bcff8bf60bf5e5be1e7f548ccf1bf6a17
Maxi Sandoval <maxisandoval98@gmail.com>
2024-10-28 16:20:09 -0300

    desaparece el btn girar una vez generado el resultado

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const opciones = ["$100", "$200", "Jackpot", "Lose", "$300", "Tenes piojitos", "
 function girarRuleta() {
     const ruleta = document.getElementById('ruleta');
     const resultado = document.getElementById('resultado');
+    const btnGirar = document.getElementById('btnGirar');
     
     const anguloGanador = 110; // El ángulo de la opción ganadora
     const vueltasCompletas = Math.floor(Math.random() * 3 + 5); // De 5 a 7 vueltas completas
@@ -13,6 +14,8 @@ function girarRuleta() {
     // Calcula la opción ganadora después de la animación
     setTimeout(() => {
         resultado.textContent = `Resultado: ${opciones[5]}`; // Siempre la opción número 3
+        btnGirar.style.display = 'none';
+        
         confetti();
     }, 6000); // Espera a que termine la animación (6 segundos)
 }
